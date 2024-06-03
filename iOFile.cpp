205844ea5f88a167a6d64bba6dce4bdb4249808d
Jaidil14 <jaidilrachmad@gmail.com>
2024-06-03 14:31:53 +0700

    Menambahkan kondisi untuk break looping

--- iOFile.cpp
+++ iOFile.cpp
@@ -16,8 +16,10 @@ int main() {
     // unlimited loop untuk menulis
     while (true) {
         cout << "- ";
-        //mendapatkan setiap karakter dalam satu baris
+        // mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
+        // loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
         
         }
     
